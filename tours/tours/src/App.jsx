a07d935133eb8cc29feac45de05a20cb2a462242
Kavindu <kavinduuni@gmail.com>
2026-03-26 14:31:13 +0530

    feat: add removeTour functionality to manage tour removal; update Tour and Tours components

--- tours/tours/src/App.jsx
+++ tours/tours/src/App.jsx
@@ -9,6 +9,11 @@ function App(){
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+
   const fetchTours  = async () => {
     setLoading(true);
     try{
@@ -29,7 +34,7 @@ function App(){
     </main>
   }
 
-  return <main><Tours tours={tours} /></main>
+  return <main><Tours tours={tours} removeTour = {removeTour} /></main>
 }
 
 export default App;
