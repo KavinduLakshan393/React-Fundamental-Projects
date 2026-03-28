c07b0e3c2402ee708b8ede2882900e93456526c6
Kavindu <kavinduuni@gmail.com>
2026-03-28 13:31:39 +0530

    feat: add structure to display user reviews

--- reviews/reviews/src/App.jsx
+++ reviews/reviews/src/App.jsx
@@ -1,18 +1,27 @@
 import { useState } from 'react';
 import people from './data';
-import { Fabeer } from 'react-icons/fa';
+import { FaChevronCircleLeft, FaChevronCircleRight, FaQuoteRight } from 'react-icons/fa';
 
 const App = () => {
   const [index, setIndex] = useState(0);
-  const [ name, job, image, text ] = people[index];
+  const { name, job, image, text } = people[index];
 
 console.log(name);
 
   return(
-    <div>
-      <h2>Reviews Starter</h2>;
-      <Fabeer class></Fabeer>
-    </div>
+    <main>
+          <article className='review'>
+            <div className='img-container'>
+              <img src={image} alt={name} className='person-img' />
+              <span className='quote-icon'>
+                <FaQuoteRight />
+              </span>
+            </div>
+            <h4 className='author'>{name}</h4>
+            <p className='job'>{job}</p>
+            <p className='info'>{text}</p>
+          </article>
+    </main>
   )
 }
 
